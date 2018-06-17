e63e15f4b4725ad48101dd6a2d765f5c07070901
chriscasella <chris.casella@gmail.com>
2018-06-17 19:15:35 -0400

    1m chart added

--- src/app/company/company.component.ts
+++ src/app/company/company.component.ts
@@ -13,6 +13,14 @@ export class CompanyComponent implements OnInit, OnChanges {
   
   companySym;
   quote;
+  greenChart = { // grey
+    backgroundColor: 'rgba(70, 255, 70, 0.664)',
+    borderColor: 'rgba(53, 189, 53, 0.938)',
+    pointBackgroundColor: 'rgba(87, 185, 74, 0.801)',
+    pointBorderColor: 'rgba(67, 163, 55, 0.801)',
+    pointHoverBackgroundColor: '#fff',
+    pointHoverBorderColor: 'rgba(148,159,177,0.8)'
+  };
   constructor(private route:ActivatedRoute,
               private StockService:StockService) { }
 
@@ -24,16 +32,21 @@ export class CompanyComponent implements OnInit, OnChanges {
         console.log('quote', res);
         this.quote = res;
       });
-      this.StockService.get1dyChart(this.companySym).subscribe(res => {
-        console.log('1dychart', res);
-        this.parseChartData('1dy', res);
+      this.StockService.get1mChart(this.companySym).subscribe( res => {
+        this.parseChartData('1m',res);
       })
+      // this.StockService.get1dyChart(this.companySym).subscribe(res => {
+      //   console.log('1dychart', res);
+      //   this.parseChartData('1dy', res);
+      // })
     })
   }
 
   parseChartData(type, data){
     if(type == '1dy'){
       this.parse1dyData(data);
+    } else {
+      this.parseElseChartData(data);
     }
   };
   parse1dyData(data){
@@ -43,28 +56,42 @@ export class CompanyComponent implements OnInit, OnChanges {
     });
     this.lineChartLabels = chartLabels;
     const chartData = data.map(element => {
-      return element.marketHigh;
+      return element.marketAverage;
     });
     const chartPayLoad = {
       data: chartData,
-      label: 'Market High'
-    };
-    const greenChart = { // grey
-      backgroundColor: 'rgba(70, 255, 70, 0.664)',
-      borderColor: 'rgba(53, 189, 53, 0.938)',
-      pointBackgroundColor: 'rgba(87, 185, 74, 0.801)',
-      pointBorderColor: 'rgba(67, 163, 55, 0.801)',
-      pointHoverBackgroundColor: '#fff',
-      pointHoverBorderColor: 'rgba(148,159,177,0.8)'
+      label: 'Average'
     };
     this.lineChartData.push(chartPayLoad);
     
         const lcd = this.lineChartData[0].data;
         console.log(lcd)
-        lcd[lcd.length-1] - lcd[0] > 0 ? this.lineChartColors.push(greenChart) : ''
+        const highMinusLow = lcd[lcd.length - 1] - lcd[0];
+        ( highMinusLow > 0) ? this.lineChartColors.push(this.greenChart) : '';
         // console.log('chartPayLoad', chartPayLoad);
 
   };
+
+  parseElseChartData(data){
+    this.lineChartData.length > 0 ? this.lineChartData = [] : '';
+    const chartLabels = data.map(element => {
+      return element.date;
+    });
+    this.lineChartLabels = chartLabels;
+    const chartData = data.map(element => {
+      return element.high;
+    });
+    const chartPayLoad = {
+      data: chartData,
+      label: 'High'
+    };
+    this.lineChartData.push(chartPayLoad);
+
+    const lcd = this.lineChartData[0].data;
+    const highMinusLow = lcd[lcd.length - 1] - lcd[0];
+    (highMinusLow > 0) ? this.lineChartColors.push(this.greenChart) : '';
+    
+  }
   public lineChartType: string = 'line';
   public lineChartLegend: boolean = true;
   public lineChartOptions: any = {
